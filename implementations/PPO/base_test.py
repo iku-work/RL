6cfde4485a3c6c9bd68f8d0f9378b4be3b6fa161
Ilya Kurinov <70485979+iku-work@users.noreply.github.com>
2023-03-04 19:57:58 +0200

    tests_of_ent_coef

--- implementations/PPO/base_test.py
+++ implementations/PPO/base_test.py
@@ -66,7 +66,7 @@ if __name__ == "__main__":
         model = PPO("MlpPolicy", env, verbose=1, tensorboard_log=log_dir, ent_coef=ent_coef)
 
         #model = PPO('MlpPolicy', env, learning_rate=param[0], clip_range=param[1], ent_coef=param[2], n_steps=param[3], n_epochs=param[4])
-        model.learn(total_timesteps=300000, tb_log_name='ppo_ent_coef_')# + str(ent_coef))#, callback=mean_reward_tracker )#, callback=clipper)
+        model.learn(total_timesteps=500000, tb_log_name='ppo_ent_coef')# + str(ent_coef))#, callback=mean_reward_tracker )#, callback=clipper)
         model.save(save_dir + '_ent_coef_' + str(ent_coef) + '_')
     
 
